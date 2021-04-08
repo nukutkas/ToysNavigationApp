7cc7999671c78342e9e7666c46c7099316848130
Nikita Kochetov <nothsmple@mail.ru>
2021-04-08 22:41:47 +0300

    EditMode added

--- ToysNavigationApp/ToyManagerViewController.swift
+++ ToysNavigationApp/ToyManagerViewController.swift
@@ -11,8 +11,20 @@ class ToyManagerViewController: UIViewController {
     
     @IBOutlet var toyNameTexfField: UITextField!
     @IBOutlet var editToyButton: UIButton!
+    @IBOutlet var newToyButton: UIButton!
+    
+    var editModeIsOn = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        newToyButton.isHidden = editModeIsOn ? true : false
+        editToyButton.isHidden = editModeIsOn ? false : true
+        
+    }
     
     @IBAction func cancelAction() {
+        dismiss(animated: true)
     }
     
 }
